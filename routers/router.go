c98a03ce6a2b074912bfa56121740c47ef560dd6
cseyhsu <cseyhsu@gmail.com>
2018-11-27 18:03:42 +0800

    增加用户下载书ç±其他用户发布的书籍的宫å功能

--- routers/router.go
+++ routers/router.go
@@ -35,7 +35,7 @@ func init() {
 			beego.NSRouter("/upload/self", &v1.UserUploadController{}, ),
 
 			// 用户发布书籍
-			beego.NSRouter("/publish",&v1.UserPublishBookController{},"post:Post"),
+			beego.NSRouter("/publish", &v1.UserPublishBookController{}, "post:Post"),
 		),
 
 	)
@@ -109,6 +109,87 @@ func init() {
 
 	})
 
+	// 用户下载其他用户发布的书籍
+	// 过滤书籍是否是违法的书籍
+	// 过滤下载书籍是否已经登录. 或者说用户的登录已经过期
+	// 判断书籍的版权 是否存在问题,
+	// 判断 书籍是需要否收费
+	// 判断用户的奖励金是否足够,扣除下载本次书籍的操作
+	beego.InsertFilter("/static/publish/:SaveName([0-9]+).pdf", beego.BeforeStatic, func(ctx *context.Context) {
+
+		// 获取文件名
+		saveName := ctx.Input.Param(":SaveName")
+		logs.Info(ctx.Request.RemoteAddr, " dl "+saveName)
+
+		// 验证 jwt
+		ok, sub := utils.ValidJWT(ctx)
+
+		if !ok {
+			// 无效 令牌, 请重新登录
+			// 或者说用户没有携带令牌的参数
+			ctx.ResponseWriter.WriteHeader(422)
+			res := struct {
+				Detail interface{} `json:"detail"`
+				Code   int         `json:"code"`
+			}{
+				Detail: "登录过期, 请重新登录",
+				Code:   422,
+			}
+
+			str, _ := utils.Marshal2JSONString(res)
+
+			ctx.WriteString(str)
+			return
+		}
+
+		// 验证用户是否上传这本书籍作为保存资料
+		btd, err := utils.GetClient().Get(sub).Bytes();
+		if err != nil {
+			ctx.ResponseWriter.WriteHeader(500)
+			ctx.WriteString("内部错误")
+			return
+		}
+		// 如果查找不到和该和用户相关的信息
+		if len(btd) == 0 {
+			ctx.ResponseWriter.WriteHeader(404)
+			ctx.WriteString("用户登录过期,请重新登录")
+			return
+		}
+
+		var info models.UserInfo
+
+		// 反序列 用户的数据
+		err = json.Unmarshal(btd, &info)
+		if err != nil {
+			ctx.ResponseWriter.WriteHeader(500)
+			ctx.WriteString("内部错误")
+			return
+		}
+
+		book := models.PublishBook{}
+		book.SaveName = saveName + ".pdf"
+		err = orm.NewOrm().Read(&book, "SaveName")
+		if err != nil {
+			logs.Info(ctx.Request.RemoteAddr, " 查找数据库错误 ", err)
+			ctx.ResponseWriter.WriteHeader(403)
+			ctx.WriteString("不允许访问")
+			return
+		}
+
+		// 如果发布的书籍不是公开的,  内容非法, 版权 不正确 不允许用户 进行下载的操作
+		if !book.Expose || !book.ContentIllegal || !book.CopyRight {
+			logs.Info(ctx.Request.RemoteAddr, "下载书籍 书籍不允许下载,可能是权限不足", )
+			ctx.ResponseWriter.WriteHeader(403)
+			ctx.WriteString("不允许访问")
+			return
+		}
+
+		//fmt.Printf("book = %+v  info = %+v \n"  , book, book.UserInfo)
+		// todo
+		//更新书籍库,, 跟新缓存 (更新用户的 奖励金的情况)
+		info.Reward -= book.Cost
+
+	})
 	// 处理出错的路由
 	beego.ErrorController(&controllers.ErrorController{})
 }
